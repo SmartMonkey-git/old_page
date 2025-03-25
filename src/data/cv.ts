e75e6fa2efce54d55aebc15264a4eee8e47b1f46
smart_monkey <rouven@propellerheads.xyz>
2025-03-25 13:12:36 +0100

    Add CV and profile pic

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -1,9 +1,23 @@
 export const experiences = [
 	{
-		company: '',
+		company: 'Propeller Heads',
 		time: '',
-		title: '',
-		location: '',
+		title: 'Machine Learning Engineer',
+		location: 'Berlin',
+		description: '',
+	},
+	{
+		company: 'Charité Berlin',
+		time: 'Sep. 2020 - Jun. 2021',
+		title: 'Master Thesis Research',
+		location: 'Berlin',
+		description: 'Conducted cutting‐edge research on AI‐driven acute kidney injury prediction, staying at the fore front of AI in health care',
+	},
+    {
+		company: 'Viscopic',
+		time: 'Jan 2018 - Sep. 2020',
+		title: '3D Developer',
+		location: 'Berlin',
 		description: '',
 	},
 	// {
@@ -17,10 +31,10 @@ export const experiences = [
 
 export const education = [
 	{
-		school: '',
-		time: '',
-		degree: '',
-		location: '',
+		school: 'BHT Berlin',
+		time: '2019 - 2021',
+		degree: 'M.Sc in Media Informatics',
+		location: 'Berlin',
 		description: '',
 	},
 	// {
@@ -33,24 +47,36 @@ export const education = [
 ];
 
 export const skills = [
-	{
-		title: '',
-		description: '',
-	},
-	// {
-	// 	title: 'Experimental Techniques',
-	// 	description: 'Spectroscopy, Isolation of Radioactive Elements, Radiation Measurement',
-	// },
+    {
+        title: 'DevOps',
+        description: 'AWS, Docker, Kubernetes, Terraform, Prefect, MLFlow, Github Actions',
+    },
+    {
+        title: 'Back-end',
+        description: 'Asyncio, MongoDB, Postgres, Redis, Kafka, SQL, Pytest, FastAPI, Pydantic',
+    },
+    {
+        title: 'Data Science',
+        description: 'Plotly, Pandas, sklearn, Jupyter, Pytorch, Numpy, Transformers, matplotlib, seaborn',
+    },
+    {
+        title: 'Programming',
+        description: 'Python, Rust, Bash',
+    },
+    {
+        title: 'Languages',
+        description: 'Deutsch, Englisch',
+    },
 ];
 
-export const publications = [
-	{
-		title: '',
-		authors: '',
-		journal: '',
-		time: '',
-		link: '',
-		abstract: '',
+//export const publications = [
+//	{
+//		title: '',
+//		authors: '',
+//		journal: '',
+//		time: '',
+//		link: '',
+//		abstract: '',
 	},
 	// {
 	// 	title: 'The Radiation of Uranium Compounds',

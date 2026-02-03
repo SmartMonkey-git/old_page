ddb7fafa90556851f2becd7b3fb13e6283aa3c2d
SmartMonkey <rouven.reuter91@gmail.com>
2026-02-03 15:54:26 +0100

    Add favicon.ico

--- src/settings.ts
+++ src/settings.ts
@@ -35,5 +35,5 @@ export const template = {
 export const seo = {
 	default_title: 'Astro Academia',
 	default_description: 'Astro Academia is a template for academic websites.',
-	default_image: '/images/astro-academia.png',
+	default_image: '/public/favicon.ico',
 }
